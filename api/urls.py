a843d7445a50ff9e744531c3b10f7278946db367
Raymundo <raymundoortega@outlook.com>
2021-12-04 13:09:52 -0500

    Listar objetos

--- api/urls.py
+++ api/urls.py
@@ -1,9 +1,12 @@
 from django.urls import path
-from .views import MrOlimpya_APIView, MrOlimpya_APIView_Detail
-
-app_name = 'api'
+from django.contrib.auth.decorators import login_required
+from .views import MrOlimpya_APIView, MrOlimpya_APIView_Detail, ListCreateApiViewMrOlimpya, home
 
 urlpatterns = [
-    path('v1/mrolimpya', MrOlimpya_APIView.as_view()), 
-    path('v1/mrolimpya/<int:pk>/', MrOlimpya_APIView_Detail.as_view()),   
+    # API
+    path('v1/mrolimpya/', ListCreateApiViewMrOlimpya.as_view()),
+    path('v1/mrolimpya/<int:pk>/', MrOlimpya_APIView_Detail.as_view()),
+
+    # Home
+    path('', login_required(home)),
 ]
--- api/views.py
+++ api/views.py
@@ -1,11 +1,18 @@
-from django.shortcuts import render
+# -*- coding: utf-8 -*-
+
+import json
+import requests
+from django.utils import formats
+from django.shortcuts import render, get_object_or_404
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from .serializers import MrOlimpyaSerializers
 from .models import MrOlimpya
-from rest_framework import status
+from rest_framework import generics, status
 from django.http import Http404
+from rest_framework.renderers import JSONRenderer
+
     
 class MrOlimpya_APIView(APIView):
     def get(self, request, format=None, *args, **kwargs):
@@ -42,3 +49,19 @@ class MrOlimpya_APIView_Detail(APIView):
         mr_olimpya = self.get_object(pk)
         mr_olimpya.delete()
         return Response(status=status.HTTP_204_NO_CONTENT)
+
+
+class ListCreateApiViewMrOlimpya(generics.ListAPIView):
+    queryset = MrOlimpya.objects.all()
+    serializer_class = MrOlimpyaSerializers
+    renderer_classes = [JSONRenderer]
+
+
+def home(request):
+    url = "http://127.0.0.1:8000/v1/mrolimpya/"
+    response = requests.get(url, timeout=30)
+    list_obj = []
+    if response.status_code == 200:
+        list_obj = json.loads(response.content)
+    print(list_obj)
+    return render(request, 'index.html', {"list_obj": list_obj})
